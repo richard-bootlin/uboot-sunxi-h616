c7a754f23717bcf85c56cb38830ecdb563e94ec5
Richard Genoud <richard.genoud@bootlin.com>
2025-08-07 16:25:32 +0200

    mtd: rawnand: sunxi_spl: fix pointer from integer without a cast
    When compiling for arm64, we get this error:
    error: passing argument 2 of ‘__memcpy_fromio’ makes pointer from
    			integer without a cast [-Wint-conversion]
    
    Fix that with a cast to (void *)(uintptr_t)
    
    Signed-off-by: Richard Genoud <richard.genoud@bootlin.com>

--- drivers/mtd/nand/raw/sunxi_nand_spl.c
+++ drivers/mtd/nand/raw/sunxi_nand_spl.c
@@ -310,7 +310,8 @@ static int nand_read_page(const struct nfc_config *conf, u32 offs,
 			return 1;
 
 		/* Retrieve the data from SRAM */
-		memcpy_fromio(data, SUNXI_NFC_BASE + NFC_RAM0_BASE,
+		memcpy_fromio(data,
+			      (void *)(uintptr_t)SUNXI_NFC_BASE + NFC_RAM0_BASE,
 			      conf->ecc_size);
 
 		/* Stop the ECC engine */
